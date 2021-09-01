1d6b8aff111c496978061518d340bdf862ef5a3c
KungPaoChick <matthewriel63@gmail.com>
2021-09-01 21:09:47 +0800

    added: installer and dependency packages.. also recommended packages that might be useful

---
+++ install.sh
@@ -0,0 +1,111 @@
+#!/bin/env bash
+set -e
+
+set_color() {
+    if [ -t 1 ]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
+    fi
+}
+
+main() {
+    set_color
+
+    reset
+    echo "${BLUE}"
+    echo "▄▄      ▄▄ ▄▄▄▄▄▄▄▄  ▄▄           ▄▄▄▄     ▄▄▄▄    ▄▄▄  ▄▄▄  ▄▄▄▄▄▄▄▄  ▄▄"; sleep 0.1
+    echo "██      ██ ██▀▀▀▀▀▀  ██         ██▀▀▀▀█   ██▀▀██   ███  ███  ██▀▀▀▀▀▀  ██"; sleep 0.1
+    echo "▀█▄ ██ ▄█▀ ██        ██        ██▀       ██    ██  ████████  ██        ██"; sleep 0.1
+    echo " ██ ██ ██  ███████   ██        ██        ██    ██  ██ ██ ██  ███████   ██"; sleep 0.1
+    echo " ███▀▀███  ██        ██        ██▄       ██    ██  ██ ▀▀ ██  ██        ▀▀"; sleep 0.1
+    echo " ███  ███  ██▄▄▄▄▄▄  ██▄▄▄▄▄▄   ██▄▄▄▄█   ██▄▄██   ██    ██  ██▄▄▄▄▄▄  ▄▄"; sleep 0.1
+    echo " ▀▀▀  ▀▀▀  ▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀     ▀▀▀▀     ▀▀▀▀    ▀▀    ▀▀  ▀▀▀▀▀▀▀▀  ▀▀"
+    echo "${RESET}"
+
+    #
+    # choose video driver
+    #
+    echo "${BOLD}##########################################################################${RESET}
+
+${RED}1.) xf86-video-amdgpu     ${GREEN}2.) nvidia     ${BLUE}3.) xf86-video-intel${RESET}     4.) Skip
+
+${BOLD}##########################################################################${RESET}"
+    read -r -p "${YELLOW}${BOLD}[!] ${RESET}Choose your video card driver. ${YELLOW}(Default: 1)${RESET}: " vidri
+
+    #
+    # prompt for installing recommended packages
+    #
+    cat recommended_pkgs.txt
+    read -p "${YELLOW}${BOLD}[!] ${RESET}Would you like to download these recommended system packages? [y/N] " recp
+
+
+    #
+    #
+    # post prompt process
+    #
+    #
+
+    # video driver card case
+    case $vidri in
+    [1])
+            DRIVER='xf86-video-amdgpu xf86-video-ati xf86-video-fbdev'
+            ;;
+
+    [2])
+            DRIVER='nvidia nvidia-settings nvidia-utils'
+            ;;
+
+    [3])
+            DRIVER='xf86-video-intel xf86-video-nouveau'
+            ;;
+
+    [4])
+            DRIVER="xorg-xinit"
+            ;;
+
+    *)
+            DRIVER='xf86-video-amdgpu xf86-video-ati xf86-video-fbdev'
+            ;;
+    esac
+
+    ## full system upgrade
+    clear
+    printf "${GREEN}${BOLD}[*] ${RESET}Performing System Upgrade and Installation...\n\n"
+    sudo pacman -Syu --noconfirm
+
+    # installing selected video driver
+    sudo pacman -S --needed --noconfirm $DRIVER
+
+    # installs package dependencies
+    sudo pacman -S --needed --noconfirm - < packages.txt
+
+    # recommended packages installer
+    if [[ "$recp" == "" || "$recp" == "N" || "$recp" == "n" ]]; then
+        printf "\n${RED}${BOLD}Abort!${RESET}\n"
+        echo "${YELLOW}${BOLD}[!] ${RESET}You can install them later by doing: ${YELLOW}(sudo pacman -S - < recommended_packages.txt)${RESET}"
+    else
+        sudo pacman -S --needed --noconfirm - < recommended_pkgs.txt
+    fi
+
+
+
+    # generate user directories
+    xdg-user-dirs-update
+
+    # copies qtile configuration
+    cp -r qtile/ $HOME/.config
+
+}
+
+main "@"
