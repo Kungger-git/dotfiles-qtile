704bf24f945f818ec5f55e9d10f5227033712e99
KungPaoChick <matthewriel63@gmail.com>
2021-09-04 18:52:09 +0800

    changed foreground of cava and tty-clock

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
     
     alias ls='ls --color=always -rthla'
     alias matrix='unimatrix -f -l ocCgGkS -s 96'
-    alias clock='tty-clock -sct -C 4'
+    alias clock='tty-clock -sct -C 1'
     alias pipes='pipes -p 5 -R -t 1 -r 0'
     #colorscript -r
 end
